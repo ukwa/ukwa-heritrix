69698cb302d2fb9bd125f1fe1bdb09b7397e80b0
Roger G. Coram <roger.coram@bl.uk>
2014-04-24 09:04:01 +0100

    Autowired serverCache.

--- src/main/java/uk/bl/wap/crawler/processor/CountryCodeAnnotator.java
+++ src/main/java/uk/bl/wap/crawler/processor/CountryCodeAnnotator.java
@@ -6,27 +6,36 @@ import org.archive.modules.CrawlURI;
 import org.archive.modules.Processor;
 import org.archive.modules.net.CrawlHost;
 import org.archive.modules.net.ServerCache;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class CountryCodeAnnotator extends Processor {
-	private final static Logger LOGGER = Logger.getLogger( CountryCodeAnnotator.class.getName() );
-	protected ServerCache serverCache;
+    private final static Logger LOGGER = Logger
+	    .getLogger(CountryCodeAnnotator.class.getName());
+    protected ServerCache serverCache;
 
-	@Override
-	protected boolean shouldProcess( CrawlURI uri ) {
-		return true;
-	}
+    @Autowired
+    public void setServerCache(ServerCache serverCache) {
+	this.serverCache = serverCache;
+    }
 
-	@Override
-	protected void innerProcess( CrawlURI curi ) {
-		try {
-			CrawlHost host = serverCache.getHostFor( curi.getUURI() );
-			String countryCode = host.getCountryCode();
-			if( countryCode != null && !countryCode.equals( "" ) && !countryCode.equals( "--" ) ) {
-				curi.getAnnotations().add( "countryCode:" + countryCode );
-			}
-		} catch( Exception e ) {
-			LOGGER.warning( e.getMessage() );
-		}
+    @Override
+    protected boolean shouldProcess(CrawlURI uri) {
+	return true;
+    }
+
+    @Override
+    protected void innerProcess(CrawlURI curi) {
+	try {
+	    CrawlHost host = serverCache.getHostFor(curi.getUURI());
+	    String countryCode = host.getCountryCode();
+	    if (countryCode != null && !countryCode.equals("")
+		    && !countryCode.equals("--")) {
+		curi.getAnnotations().add("geo:" + countryCode);
+	    }
+	} catch (Exception e) {
+	    LOGGER.warning(e.getMessage());
+	    e.printStackTrace();
 	}
+    }
 
 }
