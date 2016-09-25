ecb12223a541e8c0928e0e6c5e49a1590257b9c4
Andrew Jackson <Andrew.Jackson@bl.uk>
2016-09-25 23:17:40 +0100

    Switch to ThreadLocal Deflator instance to avoid contention

--- src/main/java/uk/bl/wap/modules/deciderules/CompressibilityDecideRule.java
+++ src/main/java/uk/bl/wap/modules/deciderules/CompressibilityDecideRule.java
@@ -3,7 +3,6 @@ package uk.bl.wap.modules.deciderules;
 import java.io.UnsupportedEncodingException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import java.util.zip.Deflater;
 
 import org.archive.modules.CrawlURI;
@@ -22,62 +21,73 @@ import org.archive.modules.deciderules.PredicatedDecideRule;
 public class CompressibilityDecideRule extends PredicatedDecideRule {
     private static final long serialVersionUID = 5661525469638017661L;
     private static final Logger LOGGER = Logger
-	    .getLogger(CompressibilityDecideRule.class.getName());
-    private final Deflater compresser = new Deflater(Deflater.BEST_SPEED);
-    {
-	setDecision(DecideResult.REJECT);
-    }
+            .getLogger(CompressibilityDecideRule.class.getName());
 
+    /*
+     * Create ThreadLocal Deflater instances to avoid contention.
+     */
+    private static final ThreadLocal<Deflater> threadLocalDeflator = new ThreadLocal<Deflater>() {
+        @Override
+        protected Deflater initialValue() {
+            return new Deflater(Deflater.BEST_SPEED);
+        }
+    };
+
+    /* Instance of Deflator */
+    private final Deflater compresser = threadLocalDeflator.get();
+
+    /* Static initialisation: */
     {
-	setMin(0.28D);
+        setDecision(DecideResult.REJECT);
+        setMin(0.28D);
     }
 
     public void setMin(double min) {
-	kp.put("min", min);
+        kp.put("min", min);
     }
 
     public double getMin() {
-	return (Double) kp.get("min");
+        return (Double) kp.get("min");
     }
 
     {
-	setMax(1.6D);
+        setMax(1.6D);
     }
 
     public void setMax(double max) {
-	kp.put("max", max);
+        kp.put("max", max);
     }
 
     public double getMax() {
-	return (Double) kp.get("max");
+        return (Double) kp.get("max");
     }
 
     @Override
     public DecideResult onlyDecision(CrawlURI uri) {
-	return this.getDecision();
+        return this.getDecision();
     }
 
     @Override
     protected boolean evaluate(CrawlURI curi) {
-	try {
-	    byte[] input = curi.getURI().getBytes("UTF-8");
-	    byte[] output = new byte[input.length + 100];
-	    int compressedDataLength;
-	    synchronized (compresser) {
-		compresser.setInput(input);
-		compresser.finish();
-		compressedDataLength = compresser.deflate(output);
-		compresser.reset();
-	    }
-	    double compressibility = ((double) compressedDataLength)
-		    / ((double) input.length);
-	    boolean result = ((compressibility < this.getMin()) || (compressibility > this
-		    .getMax()));
-	    return result;
-	} catch (UnsupportedEncodingException e) {
-	    LOGGER.log(Level.WARNING, curi.getURI(), e);
-	    curi.getNonFatalFailures().add(e);
-	}
-	return false;
+        try {
+            byte[] input = curi.getURI().getBytes("UTF-8");
+            byte[] output = new byte[input.length + 100];
+            int compressedDataLength;
+            synchronized (compresser) {
+                compresser.setInput(input);
+                compresser.finish();
+                compressedDataLength = compresser.deflate(output);
+                compresser.reset();
+            }
+            double compressibility = ((double) compressedDataLength)
+                    / ((double) input.length);
+            boolean result = ((compressibility < this.getMin())
+                    || (compressibility > this.getMax()));
+            return result;
+        } catch (UnsupportedEncodingException e) {
+            LOGGER.log(Level.WARNING, curi.getURI(), e);
+            curi.getNonFatalFailures().add(e);
+        }
+        return false;
     }
 }
