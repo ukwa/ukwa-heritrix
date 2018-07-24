660cae38616d3f73eae67fd94a41dff4a93542f7
Andrew Jackson <Andrew.Jackson@bl.uk>
2018-07-24 11:09:21 +0100

    Attempt to improve persistance of OutbackCDX.

--- src/main/java/uk/bl/wap/modules/deciderules/OutbackCDXRecentlySeenDecideRule.java
+++ src/main/java/uk/bl/wap/modules/deciderules/OutbackCDXRecentlySeenDecideRule.java
@@ -13,6 +13,7 @@ import java.util.logging.Logger;
 import org.archive.modules.CoreAttributeConstants;
 import org.archive.modules.CrawlURI;
 import org.archive.modules.recrawl.FetchHistoryHelper;
+import org.archive.modules.recrawl.RecrawlAttributeConstants;
 import org.archive.util.Reporter;
 
 import uk.bl.wap.util.OutbackCDXClient;
@@ -78,7 +79,7 @@ public class OutbackCDXRecentlySeenDecideRule
     @Override
     public boolean evaluateWithTTL(String key, CrawlURI curi, int ttl_s) {
         // Get the last crawl date and hash:
-        HashMap<String, Object> info = getInfo(curi.toString());
+        HashMap<String, Object> info = getInfo(curi.getURI());
 
         // Now determine whether we've seen this URL recently:
         Long ts;
@@ -92,12 +93,24 @@ public class OutbackCDXRecentlySeenDecideRule
                     .get(CoreAttributeConstants.A_FETCH_BEGAN_TIME);
             ts = ms_ts / 1000;
             // Also store the crawl history to enable de-duplication:
-            Map<String, Object> history = FetchHistoryHelper.getFetchHistory(
-                    curi, (Long) info.get(FetchHistoryHelper.A_TIMESTAMP), 1);
-            if (history != null)
-                history.putAll(info);
-            LOGGER.finest("Now have history: " + history);
-
+            Long o_ts = (Long) info.get(FetchHistoryHelper.A_TIMESTAMP);
+            if (o_ts != null) {
+                // This is an attempt to ensure the fetch history is persisted
+                // if this curi gets 'swapped out to disk'.
+                // This is usually done in FetchHistoryProcessor, but as we are
+                // looking up fetch history in the candidates
+                // chain we need to make sure this happens early enough. Usually
+                // this is all done during the fetch chain,
+                // but here things can linger in the Frontier, so if they get
+                // swapped out, it'll get lost.
+                curi.addPersistentDataMapKey(
+                        RecrawlAttributeConstants.A_FETCH_HISTORY);
+                Map<String, Object> history = FetchHistoryHelper
+                        .getFetchHistory(curi, o_ts, 1);
+                if (history != null)
+                    history.putAll(info);
+                LOGGER.finest("Now have history: " + history);
+            }
         }
 
         // If we've never seen this before, or if enough time has elapsed
