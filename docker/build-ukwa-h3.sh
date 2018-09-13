56457dc5a923d8a9be46e0ca24f5d366c4729776
Andrew Jackson <Andrew.Jackson@bl.uk>
2018-09-13 13:46:02 +0100

    Switch the threadsafe queues branch for testing.

--- docker/build-ukwa-h3.sh
+++ docker/build-ukwa-h3.sh
@@ -4,7 +4,7 @@ mkdir h3-build
 cd h3-build
 git clone https://github.com/ukwa/heritrix3.git
 cd heritrix3
-git checkout master
+git checkout frontier-threadsafe-workqueues
 mvn install -DskipTests
 cd ../..
 unzip h3-build/heritrix3/dist/target/heritrix-3.3.0-SNAPSHOT-dist.zip
