19872689f2ce1902ae0d6417b5fdbb1d557c8959
Andrew Jackson <Andrew.Jackson@bl.uk>
2018-09-13 14:49:13 +0100

    Emit a check so I can see which version is being used.

--- docker/build-ukwa-h3.sh
+++ docker/build-ukwa-h3.sh
@@ -5,6 +5,8 @@ cd h3-build
 git clone https://github.com/ukwa/heritrix3.git
 cd heritrix3
 git checkout frontier-threadsafe-workqueues
+echo "VERSION CHECK"
+git log -1 --name-status
 mvn install -DskipTests
 cd ../..
 unzip h3-build/heritrix3/dist/target/heritrix-3.3.0-SNAPSHOT-dist.zip
