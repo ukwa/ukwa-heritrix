ff70d07df1a1b4490831c21e9b8cc9423730c0db
Andrew Jackson <Andrew.Jackson@bl.uk>
2018-10-09 13:06:24 +0100

    Fix build and use latest updates.

--- docker/build-ukwa-h3.sh
+++ docker/build-ukwa-h3.sh
@@ -4,11 +4,16 @@ mkdir h3-build
 cd h3-build
 git clone https://github.com/ukwa/heritrix3.git
 cd heritrix3
-git checkout frontier-threadsafe-workqueues
+
+# git checkout frontier-threadsafe-workqueues
+git checkout use-latest-checkpoint
+
 echo "VERSION CHECK"
 git log -1 --name-status
-mvn install -DskipTests
-cd ../..
+mvn -s /usr/share/maven/ref/settings-docker.xml install -DskipTests
+cd contrib
+mvn -s /usr/share/maven/ref/settings-docker.xml install -DskipTests
+cd ../../..
 unzip h3-build/heritrix3/dist/target/heritrix-3.3.0-SNAPSHOT-dist.zip
 rm -fr h3-build
 
