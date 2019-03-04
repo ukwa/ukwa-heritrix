01395ecd9a60a1207b1d9b043421dea4eb389510
Andrew Jackson <Andrew.Jackson@bl.uk>
2019-03-04 14:50:24 +0000

    Switching to a 'forgetful' URI filter.

---
+++ src/main/java/uk/bl/wap/crawler/postprocessor/ForgettingFrontierProcessor.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package uk.bl.wap.crawler.postprocessor;
+
+import java.util.logging.Logger;
+
+import org.archive.crawler.datamodel.UriUniqFilter;
+import org.archive.modules.CrawlURI;
+import org.archive.modules.Processor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author Andrew Jackson <Andrew.Jackson@bl.uk>
+ *
+ */
+public class ForgettingFrontierProcessor extends Processor {
+
+    private static final Logger logger = Logger
+            .getLogger(ForgettingFrontierProcessor.class.getName());
+
+    protected UriUniqFilter uriUniqFilter;
+
+    public UriUniqFilter getUriUniqFilter() {
+        return this.uriUniqFilter;
+    }
+    @Autowired
+    public void setUriUniqFilter(UriUniqFilter uriUniqFilter) {
+        this.uriUniqFilter = uriUniqFilter;
+    }
+
+    /* (non-Javadoc)
+     * @see org.archive.modules.Processor#shouldProcess(org.archive.modules.CrawlURI)
+     */
+    @Override
+    protected boolean shouldProcess(CrawlURI curi) {
+        return true;
+    }
+
+    /* (non-Javadoc)
+     * @see org.archive.modules.Processor#innerProcess(org.archive.modules.CrawlURI)
+     */
+    @Override
+    protected void innerProcess(CrawlURI curi) throws InterruptedException {
+        logger.info("Making the frontier forget it has seen " + curi);
+        uriUniqFilter.forget(curi.getCanonicalString(), curi);
+    }
+
+}
