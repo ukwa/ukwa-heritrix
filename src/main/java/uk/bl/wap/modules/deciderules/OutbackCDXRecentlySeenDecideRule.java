8a9723367f495f0775f6f0a8c906ca1c07cb0a3a
Andrew Jackson <Andrew.Jackson@bl.uk>
2019-04-26 13:30:37 +0100

    Refactor RecentlySeen code and default to ignoring forceFetch.

--- src/main/java/uk/bl/wap/modules/deciderules/OutbackCDXRecentlySeenDecideRule.java
+++ src/main/java/uk/bl/wap/modules/deciderules/OutbackCDXRecentlySeenDecideRule.java
@@ -3,18 +3,9 @@
  */
 package uk.bl.wap.modules.deciderules;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import org.archive.modules.CoreAttributeConstants;
-import org.archive.modules.CrawlURI;
-import org.archive.modules.recrawl.FetchHistoryHelper;
-import org.archive.modules.recrawl.RecrawlAttributeConstants;
-import org.archive.util.Reporter;
 
 import uk.bl.wap.util.OutbackCDXClient;
 
@@ -23,7 +14,7 @@ import uk.bl.wap.util.OutbackCDXClient;
  *
  */
 public class OutbackCDXRecentlySeenDecideRule
-        extends RecentlySeenDecideRule implements Reporter {
+        extends RecentlySeenDecideRule {
 
     private static final long serialVersionUID = 361526253773091309L;
 
@@ -43,11 +34,14 @@ public class OutbackCDXRecentlySeenDecideRule
     public OutbackCDXRecentlySeenDecideRule() {
     }
 
-    private long errors = 0;
-    private long hits = 0;
-    private long misses = 0;
 
-    private HashMap<String, Object> getInfo(String url) {
+    /*
+     * (non-Javadoc)
+     * 
+     * @see uk.bl.wap.modules.deciderules.RecentlySeenDecideRule#getInfo()
+     */
+    @Override
+    protected HashMap<String, Object> getInfo(String url) {
         int tries = 3;
         while (tries > 0) {
             try {
@@ -69,89 +63,7 @@ public class OutbackCDXRecentlySeenDecideRule
                 }
             }
         }
-        errors++;
         return null;
-    }
-
-    /* (non-Javadoc)
-     * @see uk.bl.wap.modules.deciderules.RecentlySeenDecideRule#evaluateWithTTL(java.lang.String, org.archive.modules.CrawlURI, int)
-     */
-    @Override
-    public boolean evaluateWithTTL(String key, CrawlURI curi, int ttl_s,
-            long launch_ts) {
-        // Get the last crawl date and hash:
-        HashMap<String, Object> info = getInfo(curi.getURI());
-
-        // Now determine whether we've seen this URL recently:
-        Long ts;
-        if (info == null || !info
-                .containsKey(CoreAttributeConstants.A_FETCH_BEGAN_TIME)) {
-            misses++;
-            ts = 0l;
-        } else {
-            hits++;
-            long ms_ts = (long) info
-                    .get(CoreAttributeConstants.A_FETCH_BEGAN_TIME);
-            ts = ms_ts / 1000;
-            // Also store the crawl history to enable de-duplication:
-            Long o_ts = (Long) info.get(FetchHistoryHelper.A_TIMESTAMP);
-            if (o_ts != null) {
-                // This is an attempt to ensure the fetch history is persisted
-                // if this curi gets 'swapped out to disk'.
-                // This is usually done in FetchHistoryProcessor, but as we are
-                // looking up fetch history in the candidates
-                // chain we need to make sure this happens early enough. Usually
-                // this is all done during the fetch chain,
-                // but here things can linger in the Frontier, so if they get
-                // swapped out, it'll get lost.
-                curi.addPersistentDataMapKey(
-                        RecrawlAttributeConstants.A_FETCH_HISTORY);
-                Map<String, Object> history = FetchHistoryHelper
-                        .getFetchHistory(curi, o_ts, 1);
-                if (history != null)
-                    history.putAll(info);
-                LOGGER.finest("Now have history: " + history);
-            }
-        }
-
-        // If we've never seen this before, or if enough time has elapsed
-        // since we last saw it:
-        long currentTime = System.currentTimeMillis() / 1000;
-        if (ts == 0l || (currentTime - ts) > ttl_s || curi.forceFetch()) {
-            LOGGER.finest("Got elapsed: " + (currentTime - ts) + " versus TTL "
-                    + ttl_s + " hence NOT recently seen " + curi);
-            return false;
-        } else if (launch_ts > 0 && ts < launch_ts) {
-            LOGGER.finest("Got recrawl time: " + ts + " versus launch time "
-                    + launch_ts + " hence NOT recently seen " + curi);
-            return false;
-        } else {
-            LOGGER.finest("Has been recently seen: " + curi);
-            return true;
-        }
-    }
-
-    /* Reporter */
-
-    @Override
-    public void reportTo(PrintWriter writer) throws IOException {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void shortReportLineTo(PrintWriter pw) throws IOException {
-        pw.println("OutbackCDX Recently Seen Decide Rule Report Line");
-    }
-
-    @Override
-    public Map<String, Object> shortReportMap() {
-        return null;
-    }
-
-    @Override
-    public String shortReportLegend() {
-        return "OutbackCDX Recently Seen Decide Rule Report Legend";
     }
 
 }
--- src/main/java/uk/bl/wap/modules/deciderules/RecentlySeenDecideRule.java
+++ src/main/java/uk/bl/wap/modules/deciderules/RecentlySeenDecideRule.java
@@ -3,14 +3,22 @@
  */
 package uk.bl.wap.modules.deciderules;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.ParseException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
+import org.archive.modules.CoreAttributeConstants;
 import org.archive.modules.CrawlURI;
 import org.archive.modules.deciderules.DecideResult;
 import org.archive.modules.deciderules.PredicatedDecideRule;
+import org.archive.modules.recrawl.FetchHistoryHelper;
+import org.archive.modules.recrawl.RecrawlAttributeConstants;
 import org.archive.spring.KeyedProperties;
 import org.archive.util.DateUtils;
+import org.archive.util.Reporter;
 
 import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
@@ -19,7 +27,8 @@ import com.google.common.hash.Hashing;
  * @author Andrew Jackson <Andrew.Jackson@bl.uk>
  *
  */
-public abstract class RecentlySeenDecideRule extends PredicatedDecideRule {
+public abstract class RecentlySeenDecideRule extends PredicatedDecideRule
+        implements Reporter {
     private static final Logger LOGGER = Logger
             .getLogger(RecentlySeenDecideRule.class.getName());
 
@@ -38,12 +47,20 @@ public abstract class RecentlySeenDecideRule extends PredicatedDecideRule {
     /** Launch timestamp key if set in CrawlURI, in ms since epoch */
     public static final String LAUNCH_TIMESTAMP = "launchTimestamp";
 
+    private long errors = 0;
+    private long hits = 0;
+    private long misses = 0;
+
     // Hash function used for building keys:
     private HashFunction hf = Hashing.murmur3_128();
 
     // Whether to use the hash of the URI rather than the URI (e.g. if
     // the implementation needs short keys)
     private boolean useHashedUriKey = false;
+
+    // Whether to pay attention to the forceFetch flag and treat then as
+    // not-recently-seen.
+    private boolean obeyForceFetch = false;
 
     protected KeyedProperties kp = new KeyedProperties();
 
@@ -107,6 +124,24 @@ public abstract class RecentlySeenDecideRule extends PredicatedDecideRule {
      */
     public void setUseHashedUriKey(boolean useHashedUriKey) {
         this.useHashedUriKey = useHashedUriKey;
+    }
+
+    /**
+     * @return the obeyForceFetch
+     */
+    public boolean isObeyForceFetch() {
+        return obeyForceFetch;
+    }
+
+    /**
+     * Control whether URIs marked as 'forceFetch' will bypass the recently-seen
+     * check.
+     * 
+     * @param obeyForceFetch
+     *            the obeyForceFetch to set
+     */
+    public void setObeyForceFetch(boolean obeyForceFetch) {
+        this.obeyForceFetch = obeyForceFetch;
     }
 
     /**
@@ -211,7 +246,94 @@ public abstract class RecentlySeenDecideRule extends PredicatedDecideRule {
      * @return true if the item is new and is being added to the set of known
      *         URIs
      */
-    abstract public boolean evaluateWithTTL(String key, CrawlURI curi,
-            int ttl_s, long launch_ts);
+    private boolean evaluateWithTTL(String key, CrawlURI curi, int ttl_s,
+            long launch_ts) {
+        // Get the last crawl date and hash:
+        HashMap<String, Object> info = getInfo(curi.getURI());
+
+        // Now determine whether we've seen this URL recently:
+        Long ts;
+        if (info == null) {
+            // No info at all means it went wrong
+            errors++;
+            ts = 0l;
+        } else if (!info
+                .containsKey(CoreAttributeConstants.A_FETCH_BEGAN_TIME)) {
+            // Lookup worked but no match:
+            misses++;
+            ts = 0l;
+        } else {
+            // Match found:
+            hits++;
+            long ms_ts = (long) info
+                    .get(CoreAttributeConstants.A_FETCH_BEGAN_TIME);
+            ts = ms_ts / 1000;
+            // Also store the crawl history to enable de-duplication:
+            Long o_ts = (Long) info.get(FetchHistoryHelper.A_TIMESTAMP);
+            if (o_ts != null) {
+                // This is an attempt to ensure the fetch history is persisted
+                // if this curi gets 'swapped out to disk'.
+                // This is usually done in FetchHistoryProcessor, but as we are
+                // looking up fetch history in the candidates
+                // chain we need to make sure this happens early enough. Usually
+                // this is all done during the fetch chain,
+                // but here things can linger in the Frontier, so if they get
+                // swapped out, it'll get lost.
+                curi.addPersistentDataMapKey(
+                        RecrawlAttributeConstants.A_FETCH_HISTORY);
+                Map<String, Object> history = FetchHistoryHelper
+                        .getFetchHistory(curi, o_ts, 1);
+                if (history != null)
+                    history.putAll(info);
+                LOGGER.finest("Now have history: " + history);
+            }
+        }
+
+        // If we've never seen this before, or if enough time has elapsed
+        // since we last saw it:
+        long currentTime = System.currentTimeMillis() / 1000;
+        if (obeyForceFetch && curi.forceFetch()) {
+            LOGGER.finest(
+                    "Marked as force-fetch, hence classifying as NOT recently seen "
+                            + curi);
+            return false;
+        } else if (ts == 0l || (currentTime - ts) > ttl_s) {
+            LOGGER.finest("Got elapsed: " + (currentTime - ts) + " versus TTL "
+                    + ttl_s + " hence NOT recently seen " + curi);
+            return false;
+        } else if (launch_ts > 0 && ts < launch_ts) {
+            LOGGER.finest("Got recrawl time: " + ts + " versus launch time "
+                    + launch_ts + " hence NOT recently seen " + curi);
+            return false;
+        } else {
+            LOGGER.finest("Has been recently seen: " + curi);
+            return true;
+        }
+    }
+
+    abstract protected HashMap<String, Object> getInfo(String url);
+
+    /* Reporter */
+
+    @Override
+    public void reportTo(PrintWriter writer) throws IOException {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void shortReportLineTo(PrintWriter pw) throws IOException {
+        pw.println("Recently Seen Decide Rule Report Line");
+    }
+
+    @Override
+    public Map<String, Object> shortReportMap() {
+        return null;
+    }
+
+    @Override
+    public String shortReportLegend() {
+        return "Recently Seen Decide Rule Report Legend";
+    }
 
 }
