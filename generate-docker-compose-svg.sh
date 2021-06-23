56cb025c605ac84f92fdf963787026b2d13ab2d0
Andrew Jackson <Andrew.Jackson@bl.uk>
2021-06-24 00:50:17 +0100

    Attempted to simplify volume management. Needs further work.

--- generate-docker-compose-svg.sh
+++ generate-docker-compose-svg.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm -it -v $(pwd):/input pmsipilot/docker-compose-viz render -m graphviz -o docker-compose.svg docker-compose.yml
+docker run --rm -it -v $(pwd):/input pmsipilot/docker-compose-viz render -m graphviz --no-volumes -f -o docker-compose.svg docker-compose.yml
