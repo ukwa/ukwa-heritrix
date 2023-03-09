c1d4d899cbc7b7698cfb599d5ced7f810637e749
Andrew Jackson <Andrew.Jackson@bl.uk>
2023-03-09 15:25:27 +0000

    Add mechanism to allow well-known URIs to be overridden in the crawler beans.

--- src/main/java/uk/bl/wap/modules/extractor/ExtractorHTTPWellKnownURIs.java
+++ src/main/java/uk/bl/wap/modules/extractor/ExtractorHTTPWellKnownURIs.java
@@ -3,6 +3,8 @@
  */
 package uk.bl.wap.modules.extractor;
 
+import java.util.Arrays;
+
 import org.archive.modules.CrawlURI;
 import org.archive.modules.extractor.ExtractorHTTP;
 import org.archive.modules.extractor.Hop;
@@ -14,17 +16,37 @@ import org.archive.modules.extractor.LinkContext;
  */
 public class ExtractorHTTPWellKnownURIs extends ExtractorHTTP {
 
+    private static final Logger LOGGER = Logger
+            .getLogger(ExtractorHTTPWellKnownURIs.class.getName());
+
+    // Default set of well-known paths:
+    private String[] DEFAULT_WELL_KNOWN_PATHS = {
+        "/humans.txt",
+        "/ads.txt",
+        "/sellers.json",
+        "/.well-known/security.txt",
+        "/.well-known/host-meta.json",
+        "/.well-known/dat"
+    };
+
+    // Well-known paths that can be over-ridden at runtime:
+    protected List<String> wellKnownPaths = Arrays.asList(DEFAULT_WELL_KNOWN_PATHS);
+
+    public void setWellKnownPaths(List<String> wellKnownPaths) {
+        this.wellKnownPaths = wellKnownPaths;
+        LOGGER.fine("wellKnownPaths set to " + this.wellKnownPaths);
+    }
+
+
 	@Override
 	protected void extract(CrawlURI curi) {
 		// Do all the usual extractions:
 		super.extract(curi);
+
 		// Add some more well-known URIs:
-        addOutlink(curi, "/humans.txt", LinkContext.INFERRED_MISC, Hop.INFERRED);
-        addOutlink(curi, "/ads.txt", LinkContext.INFERRED_MISC, Hop.INFERRED);
-        addOutlink(curi, "/sellers.json", LinkContext.INFERRED_MISC, Hop.INFERRED);
-        addOutlink(curi, "/.well-known/security.txt", LinkContext.INFERRED_MISC, Hop.INFERRED);
-        addOutlink(curi, "/.well-known/host-meta.json", LinkContext.INFERRED_MISC, Hop.INFERRED);
-        addOutlink(curi, "/.well-known/dat", LinkContext.INFERRED_MISC, Hop.INFERRED);
+        for( String wellKnownPath: wellKnownPaths) {
+            addOutlink(curi, wellKnownPath, LinkContext.INFERRED_MISC, Hop.INFERRED);
+        }
 	}
 
 }
